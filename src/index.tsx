936bb40e4a81f1e5ee6ba173e7fe65177ee23ccb
vherasymenko <manqarium@gmail.com>
2023-10-31 14:13:13 +0200

    [Feature] setup  project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from 'src/App'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+)
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/index.tsx',
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: [path.resolve(__dirname), 'node_modules'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: isDevelopment ? ['react-refresh/babel'] : [],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'dist', 'index.html'),
+      filename: 'index.html',
+      templateParameters: {
+        PUBLIC_URL: isDevelopment ? '' : '/',
+      },
+      interpolate: true,
+    }),
+    ...(isDevelopment ? [new ReactRefreshWebpackPlugin()] : []),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+  },
+}
