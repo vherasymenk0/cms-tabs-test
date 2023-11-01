ba225c7e4466e89225ffff306db3dd86ae6019a8
vherasymenko <manqarium@gmail.com>
2023-11-02 00:33:31 +0200

    [Fix] remove favicon

--- webpack/copyFiles.js
+++ webpack/copyFiles.js
@@ -3,7 +3,6 @@ const fs = require('fs-extra')
 async function copyFiles() {
   try {
     await fs.copy('dist/data', 'build/data')
-    await fs.copy('dist/favicon.ico', 'build/favicon.ico')
     console.log('Files copied successfully')
   } catch (err) {
     console.error('Error copying files:', err)
