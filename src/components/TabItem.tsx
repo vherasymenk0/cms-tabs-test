63c627e1d07f3c1fa2cbdfe60661ebb181bbce33
vherasymenko <manqarium@gmail.com>
2023-11-01 16:33:59 +0200

    [Feature] create TabItem

---
+++ src/components/TabItem.tsx
@@ -0,0 +1,23 @@
+import { Suspense, lazy } from 'react'
+import { useOutletContext, useParams } from 'react-router-dom'
+import Loader from 'src/components/Loader'
+import NotFoundRedirect from 'src/components/NotFoundRedirect'
+import { TabType } from 'src/types/shared'
+
+const TabItem = () => {
+  const { id } = useParams()
+  const tabs: TabType[] = useOutletContext()
+  const currentTab: TabType[] | [] = tabs.filter((tab) => tab.id === id)
+
+  if (!currentTab[0]) return <NotFoundRedirect />
+
+  const Tab = lazy(() => import(`src/components/${currentTab[0]?.path}`))
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <Tab />
+    </Suspense>
+  )
+}
+
+export default TabItem
