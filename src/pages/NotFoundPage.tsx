efe805f6d843c847402a9b9ead8fd812cd62b4ea
vherasymenko <manqarium@gmail.com>
2023-11-01 16:34:26 +0200

    [Feature] create not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => (
+  <div>
+    This page doesn`t exist. Go <Link to="/">home</Link>
+  </div>
+)
+
+export default NotFoundPage
